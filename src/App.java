d430920b37ee32a9186011cc7181f308839d3518
Fabriccio <andreherrera27200207@gmail.com>
2025-11-19 18:27:48 +0100

    funcionF

--- src/App.java
+++ src/App.java
@@ -2,4 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    public static void eje1(){
+        System.out.println("Hola");
+    }
 }
